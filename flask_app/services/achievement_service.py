78fb400d518143741ed3182906d989be34d7d937
functionFox <thefunctionfox@gmail.com>
2026-04-16 20:32:02 -0700

    achievement service now detects changes in achievement status

--- flask_app/services/achievement_service.py
+++ flask_app/services/achievement_service.py
@@ -1,14 +1,41 @@
+import time
+
 from flask_app.models.achievement_state import AchievementState
 from flask_app.services.steam_service import SteamService
-from flask_app.services.db_service import upsert_achievements
+from flask_app.services.db_service import (
+    upsert_achievements,
+    get_achievement_state_by_app_id,
+)
+
 
 class AchievementService:
     @staticmethod
     def refresh_game_achievements(appid):
+        old_state = get_achievement_state_by_app_id(str(appid))
+
         steam = SteamService()
         state = steam.build_achievement_state(appid)
 
         upsert_achievements(str(appid), state["achievements"])
         AchievementState.save_state(state)
+
+        if old_state is not None:
+            old_lookup = {
+                achievement["apiname"]: achievement
+                for achievement in old_state["achievements"]
+            }
+
+            newly_unlocked = [
+                achievement
+                for achievement in state["achievements"]
+                if achievement["achieved"]
+                and not old_lookup.get(achievement["apiname"], {}).get("achieved", 0)
+            ]
+
+            if newly_unlocked:
+                AchievementState.save_event({
+                    "latest": newly_unlocked[-1],
+                    "timestamp": int(time.time())
+                })
 
         return state

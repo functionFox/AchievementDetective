5c1d61eca941631d0aebb7dfcbc01cb0b3c8f054
functionFox <thefunctionfox@gmail.com>
2026-04-17 00:57:23 -0700

    Open config.html by default first run

--- server.py
+++ server.py
@@ -1,5 +1,14 @@
+from threading import Timer
+import webbrowser
+
 from flask_app import app
 from flask_app.controllers import overlay_controller, api_controller
 
+
+def open_config_page():
+    webbrowser.open("http://127.0.0.1:5000/config")
+
+
 if __name__ == "__main__":
+    Timer(1.0, open_config_page).start()
     app.run(debug=True)

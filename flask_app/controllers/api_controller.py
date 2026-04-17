859ab5f9083c30e14077e2d07a10cc29b2f5e095
functionFox <thefunctionfox@gmail.com>
2026-04-16 19:44:06 -0700

    Integrating achievement service

--- flask_app/controllers/api_controller.py
+++ flask_app/controllers/api_controller.py
@@ -1,6 +1,7 @@
-from flask import jsonify
+from flask import jsonify, request
 from flask_app import app
 from flask_app.models.achievement_state import AchievementState
+from flask_app.services.achievement_service import AchievementService
 from flask_app.services.steam_service import SteamService
 
 TEST_APP_ID = 2060160  # The Farmer Was Replaced
@@ -18,3 +19,13 @@ def get_state():
 @app.route("/api/event")
 def get_event():
     return jsonify(AchievementState.load_event())
+
+@app.route("/api/achievements", methods=["GET"])
+def get_achievements():
+    appid = request.args.get("appid", type=int)
+
+    if appid:
+        state = AchievementService.refresh_game_achievements(appid)
+        return jsonify(state)
+
+    return jsonify (AchievementState.load_state())

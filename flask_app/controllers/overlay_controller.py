76fe0e3c138a853d5ccda94426a37ee83be8b5f7
functionFox <thefunctionfox@gmail.com>
2026-04-16 19:53:21 -0700

    Adding configuration page

--- flask_app/controllers/overlay_controller.py
+++ flask_app/controllers/overlay_controller.py
@@ -4,3 +4,7 @@ from flask_app import app
 @app.route("/")
 def overlay():
     return render_template("index.html")
+
+@app.route("/config")
+def config_page():
+    return render_template("config.html")
